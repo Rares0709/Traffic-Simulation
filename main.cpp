8f09e3136525c819dfc6a37ee6f7edf70ac21494
grama <Rares.Grama@test.be>
2025-02-27 14:57:49 +0100

    basis

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by grama on 2/27/2025.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,18 @@
+//
+// Created by grama on 2/27/2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+using namespace std;
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
