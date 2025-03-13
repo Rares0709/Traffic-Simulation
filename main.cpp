e9a7e7f93ebe9d3ac86a6c30dcb0c2e06bf16a0a
s0240883 <jonas.saelens@test.com>
2025-03-13 02:14:12 +0100

    1.1 verder geimplementeerd het enige dat nog moet is de foutmeldingen en de uitzonderingen

--- main.cpp
+++ main.cpp
@@ -12,20 +12,20 @@ struct Baan {
 };
 
 struct Verkeerslicht {
-    Baan baan;
-    int positie;
-    int cyclus;
+    std::string baan;
+    int positie = 0;
+    int cyclus = 0;
 };
 
 struct Voertuig {
-    Baan baan;
-    int positie;
+    std::string baan;
+    int positie = 0;
 };
 
 struct VoertuigGen {
     //vermoedelijk voor later
     Baan baan;
-    int freq;
+    int freq = 0;
 };
 
 class TrafficSim {
@@ -83,29 +83,56 @@ private:
 TrafficSim readFile(const std::string inputfile) {
     TiXmlDocument doc;
 
+    std::vector<Baan> banen;
+    std::vector<Verkeerslicht> verkeerslichten;
+    std::vector<Voertuig> voertuigen;
+
     if (!doc.LoadFile(inputfile.c_str())) {
         std::cerr << "Fout bij laden van XML: " << doc.ErrorDesc() << std::endl;
         exit(1);
     }
 
-    TiXmlElement* root = doc.FirstChildElement();
-    if(root == NULL) {
-        std::cerr << "Failed to load file: No root element." << std::endl;
-        doc.Clear();
-        exit(1);
-    }
-
-    for (TiXmlElement* elem = root->FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement()) {
+    for (TiXmlElement* elem = doc.FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement()) {
         std::string elemName = elem->Value();
         if (elemName == "VOERTUIG") {
-
+            Voertuig voertuig;
+            for (TiXmlElement* child = elem->FirstChildElement(); child != NULL; child = child->NextSiblingElement()) {
+                TiXmlText* text = child->FirstChild()->ToText();
+                std::string childName = child->Value();
+                if (childName == "baan") voertuig.baan = text->Value();
+                if (childName == "positie") voertuig.positie = std::stoi(text->Value());
+            }
+            std::cout << voertuig.baan << " " << voertuig.positie << std::endl;
+            voertuigen.push_back(voertuig);
         }
         else if (elemName == "BAAN") {
-
+            Baan baan;
+            for (TiXmlElement* child = elem->FirstChildElement(); child != NULL; child = child->NextSiblingElement()) {
+                TiXmlText* text = child->FirstChild()->ToText();
+                std::string childName = child->Value();
+                if (childName == "naam") baan.naam = text->Value();
+                if (childName == "lengte") baan.lengte = std::stoi(text->Value());
+            }
+            std::cout << baan.naam << " " << baan.lengte << std::endl;
+            banen.push_back(baan);
         }
         else if (elemName == "VERKEERSLICHT") {
-
+            Verkeerslicht verkeerslicht;
+            for (TiXmlElement* child = elem->FirstChildElement(); child != NULL; child = child->NextSiblingElement()) {
+                TiXmlText* text = child->FirstChild()->ToText();
+                std::string childName = child->Value();
+                if (childName == "baan") verkeerslicht.baan = text->Value();
+                if (childName == "positie") verkeerslicht.positie = std::stoi(text->Value());
+                if (childName == "cyclus") verkeerslicht.cyclus = std::stoi(text->Value());
+            }
+            std::cout << verkeerslicht.baan << " " << verkeerslicht.positie << " " << verkeerslicht.cyclus << std::endl;
+            verkeerslichten.push_back(verkeerslicht);
         }
     }
-
+    doc.Clear();
+    return TrafficSim(banen, verkeerslichten, voertuigen);
+}
+int main() {
+    TrafficSim traffic = readFile("test1.xml");
+    return 0;
 }
