aa5c59903cfb4abd0b18afdbfb4bfe4893fde68b
s0240883 <jonas.saelens@test.com>
2025-03-20 12:32:12 +0100

    file structuur

---
+++ src/ParseFile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jonas on 3/20/2025.
+//
+
+#include "ParseFile.h"
---
+++ src/ParseFile.h
@@ -0,0 +1,16 @@
+//
+// Created by jonas on 3/20/2025.
+//
+
+#ifndef PARSEFILE_H
+#define PARSEFILE_H
+
+
+
+class ParseFile {
+
+};
+
+
+
+#endif //PARSEFILE_H
