e59fdacbba8a4ade6c314b65471473ba3bd7064d
Ilyas-codes-2006 <185931586+Ilyas-codes-2006@users.noreply.github.com>
2025-03-20 15:38:26 +0100

    Update ParseFile.cpp

--- src/ParseFile.cpp
+++ src/ParseFile.cpp
@@ -1,5 +1,70 @@
 //
 // Created by jonas on 3/20/2025.
 //
-
 #include "ParseFile.h"
+#include "tinyxml.h"
+int Voertuig::volgendeNummer = 1;
+TrafficSim parseFile(const std::string inputfile) {
+    TiXmlDocument doc;
+
+    std::vector<Baan> banen;
+    std::vector<Verkeerslicht> verkeerslichten;
+    std::vector<Voertuig> voertuigen;
+    std::vector<VoertuigGen> voertuigengen;
+
+    if (!doc.LoadFile(inputfile.c_str())) {
+        std::cerr << "Fout bij laden van XML: " << doc.ErrorDesc() << std::endl;
+        exit(1);
+    }
+
+    for (TiXmlElement* elem = doc.FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement()) {
+        std::string elemName = elem->Value();
+        if (elemName == "VOERTUIG") {
+            Voertuig voertuig;
+            for (TiXmlElement* child = elem->FirstChildElement(); child != NULL; child = child->NextSiblingElement()) {
+                TiXmlText* text = child->FirstChild()->ToText();
+                std::string childName = child->Value();
+                if (childName == "baan") voertuig.baan = text->Value();
+                if (childName == "positie") voertuig.positie = std::stoi(text->Value());
+            }
+            //std::cout << voertuig.baan << " " << voertuig.positie << std::endl;
+            voertuigen.push_back(voertuig);
+        }
+        else if (elemName == "BAAN") {
+            Baan baan;
+            for (TiXmlElement* child = elem->FirstChildElement(); child != NULL; child = child->NextSiblingElement()) {
+                TiXmlText* text = child->FirstChild()->ToText();
+                std::string childName = child->Value();
+                if (childName == "naam") baan.naam = text->Value();
+                if (childName == "lengte") baan.lengte = std::stoi(text->Value());
+            }
+            //std::cout << baan.naam << " " << baan.lengte << std::endl;
+            banen.push_back(baan);
+        }
+        else if (elemName == "VERKEERSLICHT") {
+            Verkeerslicht verkeerslicht;
+            for (TiXmlElement* child = elem->FirstChildElement(); child != NULL; child = child->NextSiblingElement()) {
+                TiXmlText* text = child->FirstChild()->ToText();
+                std::string childName = child->Value();
+                if (childName == "baan") verkeerslicht.baan = text->Value();
+                if (childName == "positie") verkeerslicht.positie = std::stoi(text->Value());
+                if (childName == "cyclus") verkeerslicht.cyclus = std::stoi(text->Value());
+            }
+            //std::cout << verkeerslicht.baan << " " << verkeerslicht.positie << " " << verkeerslicht.cyclus << std::endl;
+            verkeerslichten.push_back(verkeerslicht);
+        }
+        if (elemName == "VOERTUIGGENERATOR") {
+            VoertuigGen voertuiggen;
+            for (TiXmlElement* child = elem->FirstChildElement(); child != NULL; child = child->NextSiblingElement()) {
+                TiXmlText* text = child->FirstChild()->ToText();
+                std::string childName = child->Value();
+                if (childName == "baan") voertuiggen.baan = text->Value();
+                if (childName == "frequentie") voertuiggen.freq = std::stoi(text->Value());
+            }
+            //std::cout << voertuiggen.baan << " " << voertuiggen.freq << std::endl;
+            voertuigengen.push_back(voertuiggen);
+        }
+    }
+    doc.Clear();
+    return TrafficSim(banen, verkeerslichten, voertuigen, voertuigengen);
+}
