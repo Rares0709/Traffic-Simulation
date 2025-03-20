59111610abb2adff156850c1ab77bf537de4a065
Ilyas-codes-2006 <185931586+Ilyas-codes-2006@users.noreply.github.com>
2025-03-20 15:38:01 +0100

    Update ParseFile.h

--- src/ParseFile.h
+++ src/ParseFile.h
@@ -5,12 +5,30 @@
 #ifndef PARSEFILE_H
 #define PARSEFILE_H
 
+#include "main.h"
+#include <cmath>
+#include <vector>
+#include "TrafficSimulation.h"
+    /**
+     * Deze functie print uit wat er moet gedaan worden in 2.1
+     * @param inputfile is de XML die wordt ingelezen om alle variabelen te verkrijgen
+     * waarmee we gaan werken.
+     */
+   /**
+    *@author Jonas Saelens
+    *@date /
+    *@version
+    */
+TrafficSim parseFile(const std::string inputfile);
 
-
-class ParseFile {
-
-};
-
+    /**
+     * Dit is de main. Hier roepen we de functie aan die alles op gang zet.
+     */
+    /**
+     *@author allemaal
+     *@date /
+     *@version /
+     */
 
 
 #endif //PARSEFILE_H
