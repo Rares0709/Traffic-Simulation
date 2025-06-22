c0ff4694e6ac8794d1d79283f8a210d566bc7eb5
s0240883 <jonas.saelens@test.com>
2025-06-22 22:38:01 +0200

    begin demo

---
+++ src/demonstration.cpp
@@ -0,0 +1,12 @@
+//
+// Created by jonas on 6/22/2025.
+//
+
+#include <gtest/gtest.h>
+#include "ParseFile.h"
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
